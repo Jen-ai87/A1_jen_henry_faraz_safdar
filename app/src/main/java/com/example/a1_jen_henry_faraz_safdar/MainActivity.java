eb0597ff5bf0b7f4ceb284b1f5343bddcecda68c
Jennifer Henry <jen.henry@georgebrown.ca>
2025-09-28 21:33:35 -0400

    set the content view and toolbar

--- app/src/main/java/com/example/a1_jen_henry_faraz_safdar/MainActivity.java
+++ app/src/main/java/com/example/a1_jen_henry_faraz_safdar/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -25,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 }
