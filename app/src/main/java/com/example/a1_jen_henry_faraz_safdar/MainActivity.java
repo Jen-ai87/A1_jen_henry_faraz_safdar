3dc66812eed39527fecbcd1d4987ef8db2f4d8f1
Jennifer Henry <jen.henry@georgebrown.ca>
2025-09-28 21:21:40 -0400

    updated main activity layout

--- app/src/main/java/com/example/a1_jen_henry_faraz_safdar/MainActivity.java
+++ app/src/main/java/com/example/a1_jen_henry_faraz_safdar/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a1_jen_henry_faraz_safdar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etHours, etRate;
+    private Button btnCalculate;
+    private TextView tvResults;
+    public static ArrayList<String> paymentList = new ArrayList<>();
+    private DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
